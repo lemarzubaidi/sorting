70c1f094f8714e7c8d717a2ea7650855a77101d1
Lemar Zubaidi <lemarzubaide9@gmail.com>
2025-06-07 18:18:50 +0300

    insertion sort in c++

---
+++ insertion sort.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+void insertionsort(int arr[], int n) {
+	int key, j;
+	for (int i = 1; i < n; i++)
+	{
+		key = arr[i];
+		j = i - 1;
+		while (j >= 0 && arr[j] > key) {
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
+}
+int main()
+{
+   
+
+
+}
+
